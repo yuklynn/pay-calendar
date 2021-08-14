c736a5919deb0eee9f57fbd4f658378abe2d8834
yuklynn <59094342+yuklynn@users.noreply.github.com>
2021-08-14 13:37:32 +0900

    メモのコントローラーを修正

--- src/app/lib/isar/memo/controller.dart
+++ src/app/lib/isar/memo/controller.dart
@@ -3,60 +3,71 @@ import 'package:isar/isar.dart';
 import '../../isar.g.dart';
 import '../../types/MemoType.dart';
 import '../../util/singleton.dart';
-import '../collections/collection.dart';
+import 'collection.dart';
 
+/// メモのIsarコントローラー
 class MemoController {
-  static Future<MemoType?> get(String id) async {
-    final isar = await CommonSingleton().isar;
+  late Isar isar; // Isarインターフェース
 
-    final collection = await isar.memoCollections.get(int.parse(id));
+  /// コンストラクタ
+  MemoController() {
+    // 初期処理
+    _init();
+  }
+
+  /// 初期処理
+  void _init() async {
+    // Isarインターフェースを作る
+    isar = await CommonSingleton().isar;
+  }
+
+  /// 1件取得
+  Future<MemoType?> get(int id) async {
+    // 1件取得する
+    final collection = await isar.memoCollections.get(id);
+
     if (collection == null) return null;
-
     return MemoType.fromCollection(collection);
   }
 
-  static Future<List<MemoType>> getByNote(String id) async {
-    final isar = await CommonSingleton().isar;
+  /// ノートIDから一覧取得
+  Future<List<MemoType>> getByNote(int id) async {
+    // ノートIDでフィルターして一覧を取得する
+    final collections =
+        await isar.memoCollections.where().filter().noteIdEqualTo(id).findAll();
 
-    final collections = await isar.memoCollections
-        .where()
-        .filter()
-        .noteIdEqualTo(int.parse(id))
-        .findAll();
-
+    // メモのデータ型のリストに変換
     final list = <MemoType>[];
     for (var collection in collections) {
       list.add(MemoType.fromCollection(collection));
     }
-
     return list;
   }
 
-  static Future<MemoType?> put(MemoType newMemo, String noteId) async {
+  /// 1件作成・更新
+  Future<MemoType?> createOrUpdate(MemoType memo, int noteId) async {
+    // メモのIsarコレクションに変換
     final collection = MemoCollection()
-      ..noteId = int.parse(noteId)
-      ..title = newMemo.title
-      ..cost = newMemo.cost
-      ..date = newMemo.date
-      ..description = newMemo.description;
+      ..noteId = noteId
+      ..title = memo.title
+      ..cost = memo.cost
+      ..date = memo.date
+      ..description = memo.description;
+    if (memo.id != null) collection.id = int.parse(memo.id!);
 
-    if (newMemo.id != null) collection.id = int.parse(newMemo.id!);
-
-    final isar = await CommonSingleton().isar;
     await isar.writeTxn((isar) async {
       await isar.memoCollections.put(collection);
     });
-
-    return await get(collection.id.toString());
+    return await get(collection.id!);
   }
 
-  static Future<bool> delete(String id) async {
-    final isar = await CommonSingleton().isar;
+  /// 1件削除
+  Future<bool> delete(int id) async {
+    var success = false;
     await isar.writeTxn((isar) async {
-      await isar.memoCollections.delete(int.parse(id));
+      success = await isar.memoCollections.delete(id);
     });
 
-    final success = (await get(id)) == null;
     return success;
   }
 }

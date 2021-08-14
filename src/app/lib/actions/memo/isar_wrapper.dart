69167c6cc29e128d6ef2e14206d6cac42c212071
yuklynn <59094342+yuklynn@users.noreply.github.com>
2021-08-14 20:18:50 +0900

    メモのアクションを修正

---
+++ src/app/lib/actions/memo/isar_wrapper.dart
@@ -0,0 +1,39 @@
+import '../../isar/memo/controller.dart';
+import '../../types/MemoType.dart';
+
+/// メモ一覧を取得する
+Future<List<MemoType>?> getList(String noteId) async {
+  final controller = MemoController();
+  try {
+    final result = controller.getByNote(int.parse(noteId));
+    return result;
+  } catch (e) {
+    // todo: エラー処理
+    print('exception: $e');
+  }
+}
+
+/// メモを作成・更新する
+Future<MemoType?> createOrUpdateMemo(MemoType memo, String noteId) async {
+  final controller = MemoController();
+  try {
+    final result = controller.createOrUpdate(memo, int.parse(noteId));
+    return result;
+  } catch (e) {
+    // todo: エラー処理
+    print('exception: $e');
+  }
+}
+
+/// メモを削除する
+Future<bool> deleteMemo(String id) async {
+  final controller = MemoController();
+  try {
+    final result = controller.delete(int.parse(id));
+    return result;
+  } catch (e) {
+    // todo: エラー処理
+    print('exception: $e');
+    return false;
+  }
+}

7cc1d36561bd6cf06ce2ca5241e6aba07eba3c53
yuklynn <59094342+yuklynn@users.noreply.github.com>
2021-09-22 01:45:00 +0900

    ファイル移動

--- src/app/lib/pages/NoteList.dart
+++ src/app/lib/pages/NoteList.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../../models/note/NotesModel.dart';
-import '../common/CommonAppBar.dart';
-import 'NoteListBody.dart';
-import 'NoteListFAB.dart';
+import '../components/common/CommonAppBar.dart';
+import '../components/note/NoteListBody.dart';
+import '../components/note/NoteListFAB.dart';
+import '../models/note/NotesModel.dart';
 
 /// ノート一覧画面
-class Notes extends StatelessWidget {
+class NoteList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(

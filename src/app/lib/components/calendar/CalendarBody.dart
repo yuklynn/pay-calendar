03bba9ac285da70a604d55fda85dba89c07b1efb
yuklynn <59094342+yuklynn@users.noreply.github.com>
2021-09-27 01:57:08 +0900

    日付表示に対応

--- src/app/lib/components/calendar/CalendarBody.dart
+++ src/app/lib/components/calendar/CalendarBody.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../models/calendar/CalendarModel.dart';
 import 'CalendarDayCell.dart';
 import 'CalendarWeekCell.dart';
 
@@ -8,13 +10,18 @@ class CalendarBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final data = context.select<CalendarModel, CalendarBodyData>(
+      (model) => CalendarBodyData(
+        dateList: model.dateListOnCurrentMonth,
+      ),
+    );
 
     return Column(
       children: [
         const SizedBox(height: 8.0),
         _buildHeader(theme.textTheme),
         const SizedBox(height: 8.0),
-        Expanded(child: _buildCalendar()),
+        Expanded(child: _buildCalendar(data.dateList)),
       ],
     );
   }
@@ -29,18 +36,31 @@ class CalendarBody extends StatelessWidget {
   }
 
   /// カレンダー部分をつくる
-  Widget _buildCalendar() {
+  Widget _buildCalendar(List<DateTime> dateList) {
     return LayoutBuilder(
       builder: (context, constraints) {
         final width = constraints.maxWidth / 7;
-        final height = constraints.maxHeight / 6;
+        final height = constraints.maxHeight / 5;
         return GridView.count(
           physics: const NeverScrollableScrollPhysics(),
           crossAxisCount: 7,
           childAspectRatio: width / height,
-          children: List.filled(7 * 6, CalendarDayCell(date: DateTime.now())),
+          children: List.generate(
+            dateList.length,
+            (index) => CalendarDayCell(
+              date: dateList[index],
+            ),
+          ),
         );
       },
     );
   }
+}
+
+class CalendarBodyData {
+  List<DateTime> dateList;
+
+  CalendarBodyData({
+    required this.dateList,
+  });
 }
--- src/app/lib/models/calendar/CalendarModel.dart
+++ src/app/lib/models/calendar/CalendarModel.dart
@@ -5,6 +5,10 @@ class CalendarModel with ChangeNotifier {
   late PageController pageController; // PageViewのコントローラー
   List<DateTime> dateRange = []; // 日付の範囲
   late DateTime currentDate; // 現在の日付
+  int currentIndex = 12;
+  List<DateTime> dateListOnPreviousMonth = []; // 前月の日付リスト
+  List<DateTime> dateListOnCurrentMonth = []; // 今月の日付リスト
+  List<DateTime> dateListOnNextMonth = []; // 来月の日付リスト
 
   /// コンストラクタ
   CalendarModel() {
@@ -17,14 +21,24 @@ class CalendarModel with ChangeNotifier {
     pageController = PageController(initialPage: 12);
 
     // 日付範囲取得
+    // 今月から前後1年間
     final today = DateTime.now();
     for (var i = 0; i < 24; i++) {
       final date = DateTime(today.year - 1, today.month + i);
       dateRange.add(date);
     }
 
-    /// 初期表示の日付は今月
-    currentDate = dateRange[12];
+    // 初期表示の日付は今月
+    currentDate = DateTime(
+      dateRange[currentIndex].year,
+      dateRange[currentIndex].month,
+      today.day,
+    );
+
+    // 前月~今月の日付リストを取得
+    _getDateListOnPreviousMonth();
+    _getDateListOnCurrentMonth();
+    _getDateListOnNextMonth();
 
     try {
       notifyListeners();
@@ -40,9 +54,77 @@ class CalendarModel with ChangeNotifier {
   /// ページ変更時の処理
   void onPageChanged(int index) {
     // 表示中の日付を変更
-    currentDate = dateRange[index];
+    currentDate = DateTime(
+      dateRange[index].year,
+      dateRange[index].month,
+      currentDate.day,
+    );
+
+    // 前月~今月の日付リストを取得
+    // 進むとき
+    if (index > currentIndex) {
+      dateListOnPreviousMonth = dateListOnCurrentMonth;
+      dateListOnCurrentMonth = dateListOnNextMonth;
+      _getDateListOnNextMonth();
+    }
+    // 戻るとき
+    else {
+      dateListOnNextMonth = dateListOnCurrentMonth;
+      dateListOnCurrentMonth = dateListOnPreviousMonth;
+      _getDateListOnPreviousMonth();
+    }
+
+    currentIndex = index;
     try {
       notifyListeners();
     } catch (_) {}
+  }
+
+  /// 前月の日付リストを取得
+  void _getDateListOnPreviousMonth() {
+    final begin = DateTime(currentDate.year, currentDate.month - 1, 1);
+    final beginWeekDay = begin.weekday % 7; // 日曜日が0になるようにする
+    final firstDay = DateTime(
+      begin.year,
+      begin.month,
+      begin.day - beginWeekDay,
+    );
+    dateListOnPreviousMonth.clear();
+    for (var i = 0; i < 7 * 5; i++) {
+      final date = DateTime(firstDay.year, firstDay.month, firstDay.day + i);
+      dateListOnPreviousMonth.add(date);
+    }
+  }
+
+  /// 来月の日付リストを取得
+  void _getDateListOnCurrentMonth() {
+    final begin = DateTime(currentDate.year, currentDate.month, 1);
+    final beginWeekDay = begin.weekday % 7; // 日曜日が0になるようにする
+    final firstDay = DateTime(
+      begin.year,
+      begin.month,
+      begin.day - beginWeekDay,
+    );
+    dateListOnCurrentMonth.clear();
+    for (var i = 0; i < 7 * 5; i++) {
+      final date = DateTime(firstDay.year, firstDay.month, firstDay.day + i);
+      dateListOnCurrentMonth.add(date);
+    }
+  }
+
+  /// 今月の日付リストを取得
+  void _getDateListOnNextMonth() {
+    final begin = DateTime(currentDate.year, currentDate.month + 1, 1);
+    final beginWeekDay = begin.weekday % 7; // 日曜日が0になるようにする
+    final firstDay = DateTime(
+      begin.year,
+      begin.month,
+      begin.day - beginWeekDay,
+    );
+    dateListOnNextMonth.clear();
+    for (var i = 0; i < 7 * 5; i++) {
+      final date = DateTime(firstDay.year, firstDay.month, firstDay.day + i);
+      dateListOnNextMonth.add(date);
+    }
   }
 }
